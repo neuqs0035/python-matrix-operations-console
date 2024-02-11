88f960dad0b7b5563cf888b0b298fa5ce09ffbb5
neuqs0035 <bhavyapadaliya179@gmail.com>
2024-02-11 17:39:53 +0530

    METHOD ADDED FOR MATRIX ADDITION AND METHOD FOR DISPLAY MATRIX IN PROPER FORMAT

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def input_matrix(row,col):
 
 	for i in range(0,row):
 		for j in range(0,col):
-			
+
 			rows.append(pyin.inputInt(prompt=f"Enter Element For Position m[{i}][{j}] : "))
 			
 		matrix.append(rows)
@@ -17,6 +17,35 @@ def input_matrix(row,col):
 
 	return matrix
 
+def add_matrix(matrix1,matrix2):
+
+	output_matrix = []
+	rows = []
+
+	for row in range(len(matrix1)):
+
+		for col in range(len(matrix1[0])):
+
+			element = matrix1[row][col] + matrix2[row][col]
+			rows.append(element)
+
+		output_matrix.append(rows)
+		rows = []
+
+	return output_matrix
+
+def display_matrix(matrix):
+
+	for row in range(len(matrix)):
+
+		for col in range(len(matrix[0])):
+
+			print(f"|   {matrix[row][col]}",end="   ")
+
+		print("|")
+
+	print(f"\nOrder = {len(matrix)} x {len(matrix[0])}\n")
+	
 def main():
 
 	print("-----------------------------")
@@ -54,6 +83,10 @@ def main():
 					matrix1 = input_matrix(m1_row_count,m1_col_count)
 					print("\nFor Matrix 2 :-")
 					matrix2 = input_matrix(m1_row_count,m1_col_count)
+
+					output_matrix = add_matrix(matrix1,matrix2)
+					print("\nOutput Matrix\n")
+					display_matrix(output_matrix)
 
 			case 0:
 				print("\nProgram Exited")
