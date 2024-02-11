143e82f3e3996ed42cb93200d51f9cef3dfa565f
neuqs0035 <bhavyapadaliya179@gmail.com>
2024-02-11 17:59:22 +0530

    METHOD ADDED FOR MATRIX SUBSTRACTION

--- main.py
+++ main.py
@@ -34,6 +34,23 @@ def add_matrix(matrix1,matrix2):
 
 	return output_matrix
 
+def substract_matrix(matrix1,matrix2):
+
+	output_matrix = []
+	rows = []
+
+	for row in range(len(matrix1)):
+
+		for col in range(len(matrix1[0])):
+
+			element = matrix1[row][col] - matrix2[row][col]
+			rows.append(element)
+
+		output_matrix.append(rows)
+		rows = []
+
+	return output_matrix
+
 def display_matrix(matrix):
 
 	for row in range(len(matrix)):
